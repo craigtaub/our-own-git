4db2e76977d2ffa92acbf325fac75f96c8c24064
Craig Taub <craigtaub@gmail.com>
2020-12-29 23:05:31 +0000

    another int tests

--- test/index.integration.spec.js
+++ test/index.integration.spec.js
@@ -50,12 +50,56 @@ describe("Integration tests", () => {
     });
   });
 
-  describe("repo:add then repo:status", () => {
-    beforeAll(async (done) => {
-      await exec("npm run repo:add one.txt two/three.txt", (err, _) => {
+  describe("repo:add", () => {
+    it("should create blob objects inside 2 char directories of content hash", async (done) => {
+      await exec("npm run repo:add one.txt two/three.txt", (err, output) => {
+        const a = fs.readFileSync(
+          "src/.repo/objects/20/edd9580b6dfe9ee477979b4aca59c44770063b",
+          {
+            encoding: "utf-8",
+          }
+        );
+        const b = fs.readFileSync(
+          "src/.repo/objects/c3/fbd8e016f0ba53befe9a3dbdadf06adab65ade",
+          {
+            encoding: "utf-8",
+          }
+        );
+
+        expect(a).toEqual("20edd9580b6dfe9ee477979b4aca59c44770063b");
+        expect(b).toEqual("c3fbd8e016f0ba53befe9a3dbdadf06adab65ade");
         done();
       });
     });
+
+    it("should update repo index, move items to staged", () => {
+      const expectedIndex = {
+        "one.txt": {
+          cwd: "20edd9580b6dfe9ee477979b4aca59c44770063b",
+          staging: "20edd9580b6dfe9ee477979b4aca59c44770063b",
+          repository: "",
+        },
+        "two/four.txt": {
+          cwd: "3596117ef1e8dba38ceeabb2101192938b6313ad",
+          staging: "",
+          repository: "",
+        },
+        "two/three.txt": {
+          cwd: "c3fbd8e016f0ba53befe9a3dbdadf06adab65ade",
+          staging: "c3fbd8e016f0ba53befe9a3dbdadf06adab65ade",
+          repository: "",
+        },
+      };
+      const index = JSON.parse(
+        fs.readFileSync("src/.repo/index", {
+          encoding: "utf-8",
+        })
+      );
+      expect(index).toEqual(expectedIndex);
+    });
+  });
+
+  describe("add then repo:status", () => {
     it("flag 1 new local changes not staged and 2 changes not comitted", async (done) => {
       await exec("npm run repo:status", (err, output) => {
         const expectedStatusAfterAdd =
@@ -66,7 +110,7 @@ describe("Integration tests", () => {
       });
     });
 
-    it("if update one.txt flag as local change", async (done) => {
+    it("if update one.txt locally flag as local change", async (done) => {
       const expectedStatusAfterOneUpdate =
         "Changed locally but not staged:\\n- one.txt\\n- two/four.txt\\n\\nStaged but not comitted:\\n- two/three.txt";
 
